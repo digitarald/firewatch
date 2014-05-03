7821d7224c8d5c9e6b80c0778f0634d3bbc86767
Harald Kirschner <hkirschner@mozilla.com>
2014-05-03 09:34:29 -0700

    Hide system apps by default Show stats for killed apps Mean and standard deviate per app

--- static/index.js
+++ static/index.js
@@ -10,16 +10,17 @@ var Apps = React.createClass({
 			device: null,
 			snapshots: [],
 			hidden: [],
+			hideSystem: true,
+			hideDead: true,
 			deviceInit: false,
 			size: {width: 640, height: 480},
-			resized: false
+			resized: false,
+			timeSpan: 60000,
+			avgSpan: 10000
 		};
 	},
 
 	handleAppInitialize: function(data) {
-		if (data.snapshots.length) {
-			this.initHiddenApps(data.snapshots[data.snapshots.length - 1]);
-		}
 		this.setState({
 			snapshots: data.snapshots,
 			device: data.device
@@ -27,9 +28,6 @@ var Apps = React.createClass({
 	},
 
 	handleAppUpdate: function(snapshot) {
-		if (this.state.deviceInit) {
-			this.initHiddenApps(snapshot);
-		}
 		var snapshots = this.state.snapshots.slice();
 		snapshots.unshift(snapshot);
 		this.setState({
@@ -43,16 +41,6 @@ var Apps = React.createClass({
 
 	handleAppDisconnected: function(error) {
 		this.setState({device: null, deviceError: error});
-	},
-
-	initHiddenApps: function(snapshot) {
-		var hidden = [];
-		_.forEach(snapshot.apps, function(app) {
-			if (app.name == 'b2g' || app.name == '(Nuwa)') {
-				hidden.push(app.id);
-			}
-		});
-		this.setState({hidden: hidden, deviceInit: false});
 	},
 
 	componentDidMount: function(root) {
@@ -103,34 +91,129 @@ var Apps = React.createClass({
 			apps: [],
 			mem: {
 				free: 0,
-				cache: 0
+				cache: 0,
+				total: 0
 			}
 		};
 		var colorScale = d3.scale.category20();
 
-		/*
-		_.sortBy(last.apps, function(app) {
-			return this.state.hidden.indexOf(app.id) > -1;
-		}, this),
-		 */
+		var hidden = this.state.hidden;
+		var avgSpan = this.state.avgSpan;
+		var hideSystem = this.state.hideSystem;
+		var hideDead = this.state.hideDead;
 
-		var apps = _.map(last.apps, function (app) {
+		var activeApps = _.pluck(last.apps, 'id');
+		var appsByPid = {};
+		var flattened = _.reduce(snapshots, function(apps, snapshot) {
+			return apps.concat(_.reject(snapshot.apps, function(app) {
+				var record = appsByPid[app.id];
+				var dead = !_.contains(activeApps, app.id)
+				if (!record) {
+					if (!app.sys || !dead) {
+						appsByPid[app.id] = {
+							meta: app,
+							created: app.time,
+							memSum: app.mem,
+							memSq: app.mem * app.mem,
+							snapshots: 1,
+							memMin: app.mem,
+							memMax: app.mem,
+							sys: app.sys,
+							dead: dead
+						};
+					}
+
+				} else {
+					record.created = app.time;
+					if (record.meta.time - avgSpan < app.time) {
+						record.memSum += app.mem;
+						record.memSq += app.mem * app.mem;
+						record.snapshots++;
+						if (record.memMin > app.mem) {
+							record.memMin = app.mem;
+						}
+						if (record.memMax < app.mem) {
+							record.memMax = app.mem;
+						}
+					}
+				}
+				return (hideSystem && app.sys) || (hideDead && app.dead) || _.contains(hidden, app.id);
+			}));
+		}, []);
+		var groupedSnapshots = _.groupBy(flattened, 'pid');
+
+		var timeRange = [
+			_.min(flattened, 'time').time,
+			_.max(flattened, 'time').time
+		];
+		var timeDrawRange = [timeRange[1] - this.state.timeSpan, timeRange[1]];
+
+		var filtered = _.filter(flattened, function(snapshot) {
+			return snapshot.time >= timeDrawRange[0] && snapshot.time <= timeDrawRange[1]
+		});
+
+		var memDrawRange = [
+			_.min(filtered, 'mem').mem,
+			_.max(filtered, 'mem').mem
+		];
+
+		var allApps = _.toArray(appsByPid);
+		_.forEach(_.sortBy(allApps, ['created']), function(entry) {
+			entry.color = colorScale(entry.meta.pid);
+		});
+
+		var apps = _.map(_.sortBy(allApps, ['dead', 'sys', 'created']), function(entry) {
+			var app = entry.meta;
 			var key = 'row-' + app.id;
 			var style = {
-				color: colorScale(app.pid)
+				color: entry.color
 			};
-			var checked = this.state.hidden.indexOf(app.id) > -1;
-			var clsName = checked ? 'hidden' : '';
+			var backgroundStyle = {
+				backgroundColor: entry.color
+			};
+			var checked = hidden.indexOf(app.id) > -1;
+			var clsName = [];
+			var disabled = false;
+			if (entry.dead) {
+				clsName.push('dead');
+				if (hideDead) {
+					clsName.push('hidden');
+					disabled = true;
+				}
+			}
+			if (hidden.indexOf(app.id) > -1) {
+				clsName.push('hidden');
+			}
+			if (app.sys) {
+				clsName.push('system');
+				if (hideSystem) {
+					clsName.push('hidden');
+					disabled = true;
+				}
+			}
+
+			var mean = entry.memSum / entry.snapshots;
+			var sd = Math.sqrt(
+				(entry.memSq - (entry.memSum * entry.memSum / entry.snapshots)) / (entry.snapshots - 1));
 			var name = (app.name.substr(0, 2) == '(P') ? '(Preallocated)' : app.name;
 			return (
-				<tr key={key} className={clsName}>
+				<tr key={key} className={clsName.join(' ')}>
 					<th style={style} title={app.pid}>
 						<label>
-							<input type='checkbox' name={app.id} checked={checked} onChange={this.handleAppCheckbox} />&nbsp;
+							<span className='icon' style={backgroundStyle} />
+							<input type='checkbox' name={app.id} checked={checked} onChange={this.handleAppCheckbox} disabled={disabled} />&nbsp;
 							<span className='name'>{name}</span>
 						</label>
 					</th>
-					<td>{app.uss} <small>Mb</small></td>
+					<td>{round(app.mem, 1)} <small>Mb</small></td>
+					<td>
+						<div>+ {round(entry.memMax, 1)}</div>
+						<div>- {round(entry.memMin, 1)}</div>
+					</td>
+					<td>
+						<div>~ {isNaN(mean) ? '0.0' : round(mean, 1)}</div>
+						<div>± {isNaN(sd) ? '0.0' : round(sd, 1)}</div>
+					</td>
 				</tr>
 			);
 		}, this);
@@ -143,22 +226,18 @@ var Apps = React.createClass({
 					</h1>
 					<table>
 						<tr>
-							<th>Physical:</th>
-							<td>{Math.round(last.mem.total + last.mem.free)} <small>Mb</small></td>
-							<th>Used:</th>
-							<td>{round(last.mem.total - last.mem.free, 1)} <small>Mb</small></td>
-						</tr>
-						<tr>
-							<th>Virtual:</th>
-							<td>{round(last.mem['used-cache'] + last.mem.cache, 1)} <small>Mb</small></td>
-							<th>Used:</th>
-							<td>{last.mem['used-cache']} <small>Mb</small></td>
+							<th>Total:</th>
+							<td>{d3.round(last.mem.total, 1)} <small>Mb</small></td>
+							<th>Free:</th>
+							<td>{d3.round(last.mem.free, 1)} <small>Mb</small></td>
+							<th>Swap:</th>
+							<td>{d3.round(last.mem.cache, 1)} <small>Mb</small></td>
 						</tr>
 					</table>
 				</header>
 				<article>
 					<div ref='graph' className='graph'>
-						<LineChart colorScale={colorScale} size={this.state.size} snapshots={snapshots} hidden={this.state.hidden} />
+						<LineChart colorScale={colorScale} size={this.state.size} timeRange={timeRange} timeDrawRange={timeDrawRange} memDrawRange={memDrawRange} groupedSnapshots={groupedSnapshots} />
 					</div>
 					<aside>
 						<table className='apps-table'>
@@ -220,55 +299,30 @@ var DataSeries = React.createClass({
 });
 
 var LineChart = React.createClass({
-	getDefaultProps: function() {
-		return {
-			timeSpan: 30000
-		}
-	},
-
 	render: function() {
-		var snapshots = this.props.snapshots;
+		var groupedSnapshots = this.props.groupedSnapshots;
+		var timeRange = this.props.timeRange;
+		var timeDrawRange = this.props.timeDrawRange;
+		var memDrawRange = this.props.memDrawRange;
 		var size = this.props.size;
-		var hidden = this.props.hidden;
-
-		var flattened = _.reduce(snapshots, function(apps, snapshot) {
-			return apps.concat(_.reject(_.values(snapshot.apps), function(app) {
-				return _.contains(hidden, app.id);
-			}));
-		}, []);
-
-		var minDataTime = _.min(flattened, 'time').time;
-		var maxDataTime = _.max(flattened, 'time').time;
-		var minDrawTime = maxDataTime - this.props.timeSpan; // Math.max(, minDataTime);
-		var maxDrawTime = maxDataTime;
-
-		var filtered = _.filter(flattened, function(snapshot) {
-			return snapshot.time >= minDrawTime && snapshot.time <= maxDrawTime
-		});
-
-		var maxDataMem = _.max(filtered, 'uss').uss;
-		var minDataMem = _.min(filtered, 'uss').uss;
-
-		var apps = _.groupBy(flattened, 'pid');
 
 		var xScale = d3.scale.linear()
 			.domain([
-				0,
-				maxDrawTime - minDrawTime
+				timeDrawRange[0],
+				timeDrawRange[1]
 			])
-			.range([size.width, 0]);
+			.range([0, size.width]);
 		var yScale = d3.scale.linear()
-			.domain([Math.max(minDataMem - 0.1, 0), maxDataMem + 0.1])
+			.domain([Math.max(memDrawRange[0] - 0.1, 0), memDrawRange[1] + 0.1])
 			.nice()
 			.range([size.height, 0]);
 		var colorScale = this.props.colorScale;
 
-		var appIndex = 0;
-		var dataSeries = _.mapValues(apps, function(snapshots, pid) {
+		var dataSeries = _.mapValues(groupedSnapshots, function(snapshots, pid) {
 			var series = _.map(snapshots, function(snapshot) {
 				return {
-					x: maxDataTime - snapshot.time,
-					y: snapshot.uss
+					x: snapshot.time,
+					y: snapshot.mem
 				};
 			});
 			pid = Number(pid);
@@ -281,7 +335,7 @@ var LineChart = React.createClass({
 
 		var xTicks = _.map(xScale.copy().ticks(5), function(tick) {
 			var left = xScale(tick);
-			var label = d3.format('.0f')(tick / 1000) + 's';
+			var label = d3.time.format('%M:%S')(new Date(tick)); // d3.round(tick / 1000, 1) + 's';
 			var key = 'xtick' + String(tick);
 			return (
 				<g key={key} className='tick'>
@@ -297,7 +351,7 @@ var LineChart = React.createClass({
 			return (
 				<g key={key} className='tick'>
 					<line x1='0' y1={top} x2={size.width} y2={top} />
-					<text x='2' y={top + 11}>{d3.format('.0f')(tick)}</text>
+					<text x='2' y={top + 11}>{tick}</text>
 				</g>
 			);
 		});
@@ -313,7 +367,7 @@ var LineChart = React.createClass({
 });
 
 function round(number, decimals) {
-	return d3.format('.0f');
+	return String(d3.round(number, 1)).replace(/^(\d+)$/, '$1.0');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
